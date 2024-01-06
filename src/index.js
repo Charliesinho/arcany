32602ac0f39da1e2f71c6c2380a83dc6c65fbf77
Charliesinho <charlietrestini@hotmail.com>
2024-01-06 23:20:34 +0100

    server twick

--- src/index.js
+++ src/index.js
@@ -710,9 +710,9 @@ async function main() {
     
     app.use(express.static( 'public' ));
 
-    const PORT  = process.env.PORT || 3000;
+    const PORT  = process.env.PORT || 5000;
 
-    server.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
       });
     
