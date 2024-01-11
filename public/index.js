0fb4917103bec5fd5fb8063c050c7264f58f5831
Charliesinho <charlietrestini@hotmail.com>
2024-01-11 19:40:10 +0100

    cape update

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 //Change this to push >
 
-const socket = io(`ws://localhost:5000`);
-// const socket = io(`https://arcany.up.railway.app/`);
+// const socket = io(`ws://localhost:5000`);
+const socket = io(`https://arcany.up.railway.app/`);
 
 //Change this to push <
 
