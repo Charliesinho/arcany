dde616807dd69c7496a686ef9473518bd876a89f
Charliesinho <charlietrestini@hotmail.com>
2025-05-16 09:15:51 +0100

    local

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 //Change this to push >
 
-// const socket = io(`ws://localhost:5000`);
-const socket = io(`https://arcanyGame.up.railway.app/`);
+const socket = io(`ws://localhost:5000`);
+//const socket = io(`https://arcanyGame.up.railway.app/`);
 // const socket = io(window.location.origin);
 
 
