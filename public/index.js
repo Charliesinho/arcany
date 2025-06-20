3cad812cee921e3babcec8032502c34d4442556e
Charliesinho <charlietrestini@hotmail.com>
2025-06-20 10:01:57 +0200

    activator for boss

--- public/index.js
+++ public/index.js
@@ -7603,8 +7603,25 @@ window.addEventListener("keydown", (e) => {
     const areaBoss = mapsInfo[currentLand].enemies.find(
       enemy => enemy.isBoss === true && enemy.active === false
     );
+    let itemToActivate = null;
+    for (const item of myPlayer.inventory) {
+      if (item.name === monsterAltar) {
+        itemToActivate = myPlayer.inventory.indexOf(item);
+      }
+    }
     if (areaBoss) {
-      activateBossEnemy(areaBoss)
+      if (itemToActivate) {
+        if ((inParty && isLeader) || !inParty)  {
+          socket.emit("toDelete", [itemToActivate]);
+          activateBossEnemy(areaBoss)
+        } else {
+        errorDisplay("Only the party leader can do offerings")
+        }
+      } else {
+        errorDisplay("You don't have the required item")
+      }
+    } else {
+      errorDisplay("There is no area boss at the moment")
     }
   }
 
@@ -9183,6 +9200,7 @@ function calculateValue(resolution) {
 function deselectUiButton() {
   // console.log("DESELECTING ALL")
   // currentDevAction = ""
+  monsterLoot.style.display = "none"
   placeWalls.style.backgroundColor = "#ffe2c1"
   deleteWalls.style.backgroundColor = "#ffe2c1"
   deleteObjButtonUi.style.backgroundColor = "#ffe2c1"
@@ -9213,6 +9231,7 @@ function deselectUiButton() {
 
 monsterAltarButtonUi.addEventListener("click", function() {
   if(currentDevAction !== "monsterAltar") {
+    monsterLoot.style.display = "flex"
   showWallsFunction(true)
 
   currentSelectedWall = null
@@ -9222,6 +9241,7 @@ monsterAltarButtonUi.addEventListener("click", function() {
 
 } else {
   showWallsFunction(false)
+  monsterLoot.style.display = "none"
   currentDevAction = "none";
   monsterAltarButtonUi.style.backgroundColor = "#ffe2c1"
  }
@@ -10443,7 +10463,7 @@ function drawColliders (type, x, y, w, h) {
           fishAvailable = true;
         }
         else if (wall.type === "monsterAltar") {
-          monsterAltar = true;
+          monsterAltar = wall.requiredItem;
         }
         else if (wall.type === "transition") {
           currentSelectedMap = wall.destination
@@ -11853,7 +11873,7 @@ function checkEnemyCombat (enemy) {
     if (enemy.drop) {
       let randomNumber = Math.floor(Math.random() * 101);
       if (enemy.dropRate >= randomNumber) {
-        socket.emit("enemyDrop", dropAmount);
+        socket.emit("enemyDrop", enemy.drop);
       }
     }
 
